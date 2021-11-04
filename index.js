af5910fc592bc7d096e45b8358d604be4f7a70c1
Nausheen Akhter <nausheen701@gmail.com>
2021-11-04 22:10:48 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+
+submitData(userName, userEmail);
+
+let submitData = {
+    userName: "Bob",
+    userEmail: "Bob123"
+  };
+  
+  let configObj = {
+      method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(submitData)
+  };
+  
+  fetch("http://localhost:3000/dogs", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      console.log(object);
+    })
+    .catch(function(error) {
+      alert("Bad things! Ragnarők!");
+      console.log(error.message);
+    });
